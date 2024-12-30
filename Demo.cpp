cfd96648a8702ab696ce8c4a3884344f025d526a
Nischay Sinha <nischey.me008@gmail.com>
2024-12-30 23:26:23 +0530

    added on 30 December 2024

---
+++ Demo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int marks;
+
+    cin >>marks;
+
+    if(marks<25){
+        cout<<"F";
+    }
+
+    if(marks >=25 && marks<=44){
+        cout<<"E";
+    }
+}
